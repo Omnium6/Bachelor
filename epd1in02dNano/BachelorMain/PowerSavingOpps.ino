8a7cb36d934dd0935fb1408d0d30994c5849beec
Omnium6 <126624616+Omnium6@users.noreply.github.com>
2025-06-03 13:32:05 +0200

    Add files via upload

---
+++ epd1in02dNano/BachelorMain/PowerSavingOpps.ino
@@ -0,0 +1,23 @@
+// Deaktiver ubrugte enheder for at spare strøm
+  // Eksempel: Sluk for USB CDC for at spare strøm, hvis du ikke bruger seriel kommunikation til debug.
+  // Dette er ofte et af de største strømforbrug i 'idle' tilstand.
+  // NRF_UARTE0->TASKS_STOPRX = 1;
+  // NRF_UARTE0->TASKS_STOPTX = 1;
+  // NRF_UARTE0->ENABLE = 0;
+  //
+  // Husk at sæt EPD i sleep / off 
+
+/* TIl EPD 
+  Du fpr et gær til at gætte hvad de gør
+  EPD_Sleep();
+  EPD_TurnOnDisplay();
+*/
+
+/*
+Slukker LED på boardet
+pinMode(LED_PWR, OUTPUT);
+digitalWrite(LED_PWR, LOW);
+
+Skal bruge Arduino Low Power bib
+LowPower.deepSleep(); // Eller LowPower.sleep() for en lettere dvale
+*/
